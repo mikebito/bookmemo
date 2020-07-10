e7b749819f8894648c582235cc873f438c53624c
mikebito <30741054+mikebito@users.noreply.github.com>
2020-07-10 19:00:06 +0900

    Initial Commit

---
+++ memopads/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memopads
+//
+//  Created by 中原幹人 on 2020/07/10.
+//  Copyright © 2020 Mikito Nakahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
